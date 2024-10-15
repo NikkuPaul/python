8f39f34889673e1d88f9fe13373f76232eff6faa
Nikhil <paulnikhil881@gmail.com>
2024-10-15 20:40:11 +0530

    Adding Files

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string user_name:
+    cout << "What is your name" << std::endl;
+    cin >> user_name >> std::endl;
+    cout << user_name << std::endl;
+    return 0;
+}
---
+++ hello.py
@@ -0,0 +1,6 @@
+
+user_name = input("what is your name ?")
+user_age = input("what is your age?")
+
+print("Welcome " + user_name + " to python programming!")
+print("Your age is " + user_age)
